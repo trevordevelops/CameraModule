cf36c562791cc55ba9e1c0922919687d0a09384d
Trevor <85640211+trevordevelops@users.noreply.github.com>
2022-09-02 10:21:34 -0700

    Initial Commit

---
+++ CameraModule/CameraModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraModuleApp.swift
+//  CameraModule
+//
+//  Created by Trevor Welsh on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
